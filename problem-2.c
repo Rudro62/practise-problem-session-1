aa3419392c88551f90f2f935d75011c98eedf939
Rudro62 <rudrohasan62@gmail.com>
2023-04-03 20:53:38 +0600

    second problem solved

---
+++ problem-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a=5;
+    int b=2;
+    int plus=a+b;
+    // int minus= a-b;
+    // int multi= a*b;
+    // int dvd= a/b;
+    printf("%d", plus);
+    return 0;
+}
